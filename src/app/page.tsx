529ec6e79cf61c0040416a8f21e3fb09e228f99a
Tarun <sr.tarun98@gmail.com>
2026-08-13 18:11:54 +0530

    join us, support us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <Significance />
         <MessageSection />
         <Gallery />
-        <JoinSection />
+        {/* <JoinSection /> */}
         <JoinMission />
         <SupportUs />
       </main>
